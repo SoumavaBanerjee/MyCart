34889d46d4934c889d2fc4fa841a5fa6905fb204
SoumavaBanerjee <soumava.rivu@gmail.com>
2021-05-05 21:21:39 +0530

    mock product api created

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import morgan from "morgan";
 
 import { homeRouter, productRouter } from "./routes/";
 
@@ -9,6 +10,7 @@ const PORT = 5000;
 
 app.use(cors());
 app.use(helmet());
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- server/src/routes/product.ts
+++ server/src/routes/product.ts
@@ -7,4 +7,14 @@ router.get("/products", (req: Request, res: Response, next: NextFunction) => {
   return res.json(mockProducts);
 });
 
+router.get(
+  "/products/:id",
+  (req: Request, res: Response, next: NextFunction) => {
+    const product = mockProducts.find(
+      (product) => product._id === req.params.id
+    );
+    return res.json(product);
+  }
+);
+
 export { router as productRouter };
