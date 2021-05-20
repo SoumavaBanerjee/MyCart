aa53a2abd0ecba6809767c6ebd8471c7a9a924dc
SoumavaBanerjee <soumava.rivu@gmail.com>
2021-05-21 00:17:54 +0530

    unit tests for auth

--- server/tests/model/user.spec.ts
+++ server/tests/model/user.spec.ts
@@ -1,10 +1,10 @@
 import { expect } from "chai";
 import User from "../../src/model/user.model";
-import { IUser } from "../../src/interface";
+import { IUserDoc } from "../../src/interface";
 
 describe("user model", () => {
   it("should be invalid if name is empty", (done) => {
-    const user: IUser = new User();
+    const user: IUserDoc = new User();
     user.validate((err) => {
       expect(err?.name).to.exist;
       done();
@@ -12,7 +12,7 @@ describe("user model", () => {
   });
 
   it("should have validation error if email is not in correct format", (done) => {
-    const user: IUser = new User({
+    const user: IUserDoc = new User({
       name: "Soumava Banerjee",
       email: "abc.com",
       password: "123",
---
+++ server/tests/routes/user.spec.ts
@@ -0,0 +1,31 @@
+process.env.NODE_ENV = "test";
+import chai from "chai";
+import chaiHttp from "chai-http";
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+describe("user  route", () => {
+  // beforeEach((done) => {
+  //   Product.deleteMany({}, (err) => done());
+  // });
+
+  describe("/POST login", () => {
+    it("should GET all products", (done) => {
+      chai
+        .request("http://localhost:5000")
+        .post("/api/users/login")
+        .send({
+          email: "frost.byte@xyz.com",
+          password: "frost1234",
+        })
+        .end((err, res) => {
+          console.log(res);
+          expect(res.status).to.equal(200);
+          expect(err).to.not.exist;
+          expect(res.body).to.have.property("token");
+          done();
+        });
+    });
+  });
+});
