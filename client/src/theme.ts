5204a2b1ebee0abc4071c901f73a0bdb02923e25
SoumavaBanerjee <soumava.rivu@gmail.com>
2021-06-05 16:27:08 +0530

    changed typography and general theme

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -11,6 +11,38 @@ const theme = createMuiTheme({
       main: red[200],
     },
   },
+  typography: {
+    fontFamily: "Lato",
+    h1: {
+      fontFamily: "Prata",
+    },
+    h2: {
+      fontFamily: "Prata",
+    },
+    h3: {
+      fontFamily: "Prata",
+    },
+    h4: {
+      fontFamily: "Prata",
+    },
+    h5: {
+      fontFamily: "Prata",
+    },
+    h6: {
+      fontFamily: "Prata",
+    },
+    subtitle1: {
+      fontFamily: "Lato",
+    },
+  },
+  props: {
+    MuiTooltip: {
+      arrow: true,
+    },
+  },
+  shape: {
+    borderRadius: 10,
+  },
   overrides: {
     MuiStepper: {
       root: {
