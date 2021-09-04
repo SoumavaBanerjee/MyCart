d0aff8d87bb01132b0f4b81e68c7322b6aecad02
SoumavaBanerjee <soumava.rivu@gmail.com>
2021-09-04 17:15:38 +0530

    [NEW] add product and update product routes

--- server/src/controllers/product.controller.ts
+++ server/src/controllers/product.controller.ts
@@ -4,6 +4,11 @@ import asyncHandler from "express-async-handler";
 import Product from "../model/product.model";
 import { IProduct } from "../interface";
 
+/**
+ * @description  get all product
+ * @route PUT /api/products/
+ * @public
+ */
 export const getAllProducts = asyncHandler(
   async (req: Request, res: Response) => {
     const products: IProduct[] = await Product.find();
@@ -12,6 +17,12 @@ export const getAllProducts = asyncHandler(
     res.status(200).json(products);
   }
 );
+
+/**
+ * @description  get single product
+ * @route PUT /api/products/:id
+ * @public
+ */
 
 export const getProduct = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -22,5 +33,56 @@ export const getProduct = asyncHandler(
     }
 
     res.status(200).json(product);
+  }
+);
+
+/**
+ * @description  get single product
+ * @route POST /api/products
+ * @admin
+ */
+export const createProduct = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const product: IProduct = new Product({
+      user: req.user?._id,
+      name: "Sample Product",
+      image: "/images/sample.jpg",
+      description: "Sample Description",
+      brand: "dummy brand",
+      category: "dummy cat",
+      price: 0,
+      countInStock: 0,
+      rating: 1,
+      numReviews: 1,
+    });
+
+    const createdProduct = await product.save();
+    res.status(201).json(createdProduct);
+  }
+);
+/**
+ * @description  update a product
+ * @route PUT /api/products/:id
+ * @admin
+ */
+export const updateProduct = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const product: IProduct | null = await Product.findById(req.params.id);
+
+    if (product) {
+      product.name = req.body.name || product.name;
+      product.description = req.body.description || product.description;
+      product.image = req.body.image || product.image;
+      product.price = req.body.price || product.price;
+      product.countInStock = req.body.countInStock || product.countInStock;
+      product.brand = req.body.brand || product.brand;
+      product.category = req.body.category || product.category;
+
+      const updatedProduct = await product.save();
+      res.send(updatedProduct);
+    } else {
+      res.status(404);
+      throw new Error("No Product found");
+    }
   }
 );
--- server/src/routes/product.ts
+++ server/src/routes/product.ts
@@ -1,9 +1,21 @@
 import express from "express";
-import { getAllProducts, getProduct } from "../controllers/product.controller";
+import {
+  getAllProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+} from "../controllers/product.controller";
+import { isAdmin, verifyToken } from "../middlewares/auth";
 
 const router = express.Router();
 
-router.get("/products", getAllProducts);
-router.get("/products/:id", getProduct);
+router
+  .route("/products")
+  .get(getAllProducts)
+  .post(verifyToken, isAdmin, createProduct);
+router
+  .route("/products/:id")
+  .get(getProduct)
+  .put(verifyToken, isAdmin, updateProduct);
 
 export { router as productRouter };
